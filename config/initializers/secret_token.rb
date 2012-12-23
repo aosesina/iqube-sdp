44a1cf58637d08d72fbbc5e58bd4177e2506e9c6
e911miri <e911miri@ubuntu.ubuntu-domain>
2012-12-23 13:54:53 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sdp::Application.config.secret_token = '953802f16c890d7f774951a4ccdbb22e1ef1e0667bd440047b915b2ed9a7f210fa6345f8aead9b3c55ab55dbe363600cf8bbf80204bebcbe2988acba8a1717aa'
