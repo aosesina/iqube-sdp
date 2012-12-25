eb7e839a745b8ce1808f8a3af936f77f2160ee94
Tomiwa <e911miri@gmail.com>
2012-12-25 22:45:51 +0100

    some changes to studentview

--- app/controllers/studentview_controller.rb
+++ app/controllers/studentview_controller.rb
@@ -1,6 +1,7 @@
 class StudentviewController < ApplicationController
   before_filter :authenticate_student!
   def index
-    
+    @started_courses=current_user.subscriptions.where(:startdate=>nil)
+      
   end
 end
