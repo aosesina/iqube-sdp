1fb2cb94262373dbd7a2a542124b3be1f1066251
Tomiwa <e911miri@gmail.com>
2012-12-27 22:21:08 +0100

    Cleaning out for the night, UI changes etc

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,6 @@ class Course < ActiveRecord::Base
   attr_accessible :code, :duration_in_weeks, :price, :summary, :title, :image
   has_many :subscriptions
   has_many :users, :through=> :subscriptions
+  
+  validates :duration_in_weeks, :code, :price, :title, presence: true
 end
